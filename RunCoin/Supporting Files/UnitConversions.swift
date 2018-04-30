478340e62ae58179ffcffab106c1015bf386a5e5
Roland Christensen <RolandChristensen@rolands-macbook-pro.local>
2018-04-30 11:48:18 -0600

    Make constraints with stack views for Start run/home screen.

---
+++ RunCoin/Supporting Files/UnitConversions.swift
@@ -0,0 +1,9 @@
+//
+//  UnitConversions.swift
+//  RunCoin
+//
+//  Created by Roland Christensen on 4/30/18.
+//  Copyright © 2018 Roland Christensen. All rights reserved.
+//
+
+import Foundation
--- RunCoin/Views/StartRunViewController.swift
+++ RunCoin/Views/StartRunViewController.swift
@@ -12,6 +12,51 @@ import GoogleSignIn
 
 class StartRunViewController: UIViewController {
     
+    //Declared Variables
+    private var run : Run?
+    @IBOutlet weak var metricsStackView: UIStackView!
+    @IBOutlet weak var launchPrompt: UIStackView!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    //Buttons & Actions
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Finished Running?",
+                                                message: "Do you want to stop your workout?",
+                                                preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alertController.addAction(UIAlertAction(title: "Save", style: .default) { _ in
+            self.stopRun()
+            //self.performSegue(withIdentifier: .details, sender: nil)
+        })
+        alertController.addAction(UIAlertAction(title: "Discard", style: .destructive) { _ in
+            self.stopRun()
+            _ = self.navigationController?.popToRootViewController(animated: true)
+        })
+        
+        present(alertController, animated: true)
+    }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        startRun()
+    }
+    
+    private func startRun() {
+        metricsStackView.isHidden = false
+        launchPrompt.isHidden = true
+        startButton.isHidden = true
+        stopButton.isHidden = false
+    }
+    
+    private func stopRun() {
+        metricsStackView.isHidden = true
+        launchPrompt.isHidden = false
+        startButton.isHidden = false
+        stopButton.isHidden = true
+    }
+    
+    
+    
     @IBAction func logoutButtonPressed(_ sender: UIButton) {
         GIDSignIn.sharedInstance().signOut()
         let firebaseAuth = Auth.auth()
@@ -26,8 +71,8 @@ class StartRunViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        metricsStackView.isHidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,4 +85,15 @@ class StartRunViewController: UIViewController {
         let signInVC = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
         self.present(signInVC, animated: true, completion: nil)
     }
+    
+    
+    
+    
+    
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let destination = segue.destination as! RunStatsViewController
+//
+//
+//    }
 }
